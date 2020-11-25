d886b800c71cb000b5b134a348e82da00d15e38f
hdj <hdj@addcn.com>
2020-11-25 10:53:29 +0800

    format

--- publish/config_any.php
+++ publish/config_any.php
@@ -9,21 +9,12 @@ declare(strict_types=1);
  * @contact  huangdijia@gmail.com
  * @license  https://github.com/huangdijia/hyperf-config-array/blob/main/LICENSE
  */
-use Huangdijia\ConfigArray\Source\DemoSource;
 
-/*
- * This file is part of Smsease.
- *
- * @link     https://github.com/huangdijia/hyperf-config-any
- * @document https://github.com/huangdijia/hyperf-config-any/blob/main/README.md
- * @contact  huangdijia@gmail.com
- * @license  https://github.com/huangdijia/hyperf-config-any/blob/main/LICENSE
- */
 return [
     'enable'                 => env('CONFIG_ANY_ENABLE', false),
     'interval'               => env('CONFIG_ANY_INTERVAL', 5),
     'use_standalone_process' => true,
-    'source'                 => DemoSource::class,
+    'source'                 => \Huangdijia\ConfigArray\Source\DemoSource::class,
     'mapping'                => [
         // source => target
         'bar.foo' => 'bar.foo',
---
+++ src/Source/DemoSource.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of hyperf-config-array.
+ *
+ * @link     https://github.com/huangdijia/hyperf-config-array
+ * @document https://github.com/huangdijia/hyperf-config-array/blob/main/README.md
+ * @contact  huangdijia@gmail.com
+ * @license  https://github.com/huangdijia/hyperf-config-array/blob/main/LICENSE
+ */
+namespace Huangdijia\ConfigArray\Source;
+
+use Huangdijia\ConfigArray\SourceInterface;
+
+class DemoSource implements SourceInterface
+{
+    public function toArray(): array
+    {
+        return [
+            'bar' => [
+                'foo' => 'some value',
+            ],
+        ];
+    }
+}
