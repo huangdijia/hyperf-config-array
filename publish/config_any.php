7f3029f9cf51eeb51388816a36ef858ba9209a52
hdj <hdj@addcn.com>
2020-11-25 10:52:41 +0800

    format

--- publish/config_any.php
+++ publish/config_any.php
@@ -1,10 +1,17 @@
 <?php
 
 declare(strict_types=1);
-
+/**
+ * This file is part of hyperf-config-array.
+ *
+ * @link     https://github.com/huangdijia/hyperf-config-array
+ * @document https://github.com/huangdijia/hyperf-config-array/blob/main/README.md
+ * @contact  huangdijia@gmail.com
+ * @license  https://github.com/huangdijia/hyperf-config-array/blob/main/LICENSE
+ */
 use Huangdijia\ConfigArray\Source\DemoSource;
 
-/**
+/*
  * This file is part of Smsease.
  *
  * @link     https://github.com/huangdijia/hyperf-config-any
--- src/SourceInterface.php
+++ src/SourceInterface.php
@@ -2,12 +2,12 @@
 
 declare(strict_types=1);
 /**
- * This file is part of Smsease.
+ * This file is part of hyperf-config-array.
  *
- * @link     https://github.com/huangdijia/hyperf-config-any
- * @document https://github.com/huangdijia/hyperf-config-any/blob/main/README.md
+ * @link     https://github.com/huangdijia/hyperf-config-array
+ * @document https://github.com/huangdijia/hyperf-config-array/blob/main/README.md
  * @contact  huangdijia@gmail.com
- * @license  https://github.com/huangdijia/hyperf-config-any/blob/main/LICENSE
+ * @license  https://github.com/huangdijia/hyperf-config-array/blob/main/LICENSE
  */
 namespace Huangdijia\ConfigArray;
 
