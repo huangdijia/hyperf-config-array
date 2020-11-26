b241489fb43b1f53512b9eab7428cb4fe2b354c2
huangdijia <huangdijia@gmail.com>
2020-11-26 09:04:51 +0800

    add tests

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -5,11 +5,11 @@ namespace Huangdijia\ConfigArray\Tests;
 use Hyperf\Testing\Client;
 use Psr\Container\ContainerInterface;
 
-class ConfigTest extends TestCase
+class ExampleTest extends TestCase
 {
     public function testExample()
     {
-        $content = file_get_contents('http://localhost:9501');
+        $content = file_get_contents('http://127.0.0.1:9501');
 
         $this->assertSame(date('Y-m-d'), $content);
     }
