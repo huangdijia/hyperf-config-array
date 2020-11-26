acf50df272ce8bccff0a5364e29fa50695c47745
huangdijia <huangdijia@gmail.com>
2020-11-26 09:29:51 +0800

    add tests

--- publish/config_array.php
+++ publish/config_array.php
@@ -10,7 +10,7 @@ declare(strict_types=1);
  * @license  https://github.com/huangdijia/hyperf-config-array/blob/main/LICENSE
  */
 return [
-    'enable'                 => env('CONFIG_ARRAY_ENABLE', false),
+    'enable'                 => env('CONFIG_ARRAY_ENABLE', true),
     'interval'               => env('CONFIG_ARRAY_INTERVAL', 5),
     'use_standalone_process' => env('CONFIG_ARRAY_USE_STANDALONE_PROCESS', true),
     'source'                 => \Huangdijia\ConfigArray\Source\DemoSource::class,
