106f37a0ef14de55ff07878ca38b3054fb46d24c
huangdijia <huangdijia@gmail.com>
2020-11-26 10:07:10 +0800

    add github  actions icon

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -22,14 +22,6 @@ class ExampleTest extends TestCase
         $url = 'http://127.0.0.1:9501';
         $content = file_get_contents($url);
 
-        // $curl = curl_init();
-        // curl_setopt($curl, CURLOPT_URL, $url);
-        // curl_setopt($curl, CURLOPT_HEADER, 0);
-        // curl_setopt($curl, CURLOPT_TIMEOUT, 1);
-        // curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-
-        // $content = curl_exec($curl);
-
         $this->assertSame(date('Y-m-d'), $content);
     }
 }
