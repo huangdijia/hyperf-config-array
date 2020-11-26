d52e92f96b361291f8f26b0e79b96d276bea96d0
huangdijia <huangdijia@gmail.com>
2020-11-26 09:17:55 +0800

    add tests

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -1,15 +1,34 @@
 <?php
 
+declare(strict_types=1);
+/**
+ * This file is part of hyperf-config-array.
+ *
+ * @link     https://github.com/huangdijia/hyperf-config-array
+ * @document https://github.com/huangdijia/hyperf-config-array/blob/main/README.md
+ * @contact  huangdijia@gmail.com
+ * @license  https://github.com/huangdijia/hyperf-config-array/blob/main/LICENSE
+ */
 namespace Huangdijia\ConfigArray\Tests;
 
-use Hyperf\Testing\Client;
-use Psr\Container\ContainerInterface;
-
+/**
+ * @internal
+ * @coversNothing
+ */
 class ExampleTest extends TestCase
 {
     public function testExample()
     {
-        $content = file_get_contents('http://127.0.0.1:9501');
+        $url = 'http://127.0.0.1:9501';
+        // $content = file_get_contents($curl);
+
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_HEADER, 0);
+        curl_setopt($curl, CURLOPT_TIMEOUT, 1);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+
+        $content = curl_exec($curl);
 
         $this->assertSame(date('Y-m-d'), $content);
     }
