b96a44d6dea4bf378a60bbb403bdd729be02d802
huangdijia <huangdijia@gmail.com>
2020-11-26 09:33:52 +0800

    add tests

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -20,15 +20,15 @@ class ExampleTest extends TestCase
     public function testExample()
     {
         $url = 'http://127.0.0.1:9501';
-        // $content = file_get_contents($curl);
+        $content = file_get_contents($url);
 
-        $curl = curl_init();
-        curl_setopt($curl, CURLOPT_URL, $url);
-        curl_setopt($curl, CURLOPT_HEADER, 0);
-        curl_setopt($curl, CURLOPT_TIMEOUT, 1);
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        // $curl = curl_init();
+        // curl_setopt($curl, CURLOPT_URL, $url);
+        // curl_setopt($curl, CURLOPT_HEADER, 0);
+        // curl_setopt($curl, CURLOPT_TIMEOUT, 1);
+        // curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
 
-        $content = curl_exec($curl);
+        // $content = curl_exec($curl);
 
         $this->assertSame(date('Y-m-d'), $content);
     }
